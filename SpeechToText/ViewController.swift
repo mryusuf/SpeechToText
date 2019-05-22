35b928fc85943e26e94611e92a65a270e0b9c85f
Muhamad Yusuf Indra <muhamadyusufindra@gmail.com>
2019-05-22 15:06:02 +0700

    Initial Commit

---
+++ SpeechToText/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeechToText
+//
+//  Created by Indra Permana on 22/05/19.
+//  Copyright © 2019 Yusuf Indra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
